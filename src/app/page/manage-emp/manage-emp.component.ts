8665fdcf20095a9df7a9deec692695ccbf2ced51
pr23106082-Yesith <hansana.etec1216@example.com>
2024-07-22 01:02:12 +0530

    ng Model

---
+++ src/app/page/manage-emp/manage-emp.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-manage-emp',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './manage-emp.component.html',
+  styleUrl: './manage-emp.component.css'
+})
+export class ManageEmpComponent {
+
+public firstName : string = ""
+
+  addemployee(){
+    alert(this.firstName)
+  }
+}
